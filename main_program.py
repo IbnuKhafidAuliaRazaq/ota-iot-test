6a0898ba214a3d4be36af444cbe57606609b2cf4
IbnuKhafidAuliaRazaq <70868060+IbnuKhafidAuliaRazaq@users.noreply.github.com>
2026-02-05 13:52:57 +0700

    Implement LED control in loop function
    Added LED control to the loop function with on/off states.

--- main_program.py
+++ main_program.py
@@ -1,5 +1,13 @@
 import time
+import machine
+
+led = machine.Pin(2, machine.Pin.OUT)
 
 def loop():
     print("test2")
-    time.sleep(2)
+
+    led.off()   # mati
+    time.sleep(1)
+
+    led.on()    # hidup
+    time.sleep(1)
